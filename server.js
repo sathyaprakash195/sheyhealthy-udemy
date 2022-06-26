9b94c8f3b8cd7ea3d6f2f03bb44fda609abdb654
sathyaprakash195 <sathyaprakash195@gmail.com>
2022-06-26 14:44:18 +0530

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
@@ -8,19 +8,18 @@ const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 const path = require("path");
 
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/doctor', doctorRoute);
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use('/' . express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    }
-    );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
 }
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Node server started at port ${port}`));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
